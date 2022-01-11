feb28b411378d4dd64b1528db06f5a342e61e24a
凉风有信 <3042146237@qq.com>
2022-01-11 10:35:34 +0800

    82 && 83

---
+++ 82-删除排序链表中的重复元素2/index.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  // 判断是否存在
+  if (!head) return head
+  // new 一个虚拟头节点
+  var hair = new ListNode(-1, head), pre = hair, cur = head
+  while(cur && cur.next) {
+    // 如果cur不等于cur的下一位，将cur和pre均往后移动一位
+    if(cur.val !== cur.next.val) {
+      pre = pre.next
+      cur = cur.next
+    } else {
+      // 如果cur等于cur的下一位，则只移动cur，直到cur的下一位和cur不相等为止
+      while(cur && cur.next && (cur.val === cur.next.val)) {
+        cur = cur.next
+      }
+      // 然后把pre的下一位指向cur的下一位，并把cur往后移一位
+      pre.next = cur.next
+      cur = cur.next
+    }
+  }
+
+  return hair.next
+};
---
+++ 83-删除排序链表中的重复元素/index.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (!head) return head
+  var pre = head, cur = head.next
+  while(cur) {
+    if(pre.val === cur.val) {
+      cur = cur.next
+      pre.next = cur
+    } else {
+      cur = cur.next
+      pre = pre.next
+    }
+  }
+
+
+  return head
+};
