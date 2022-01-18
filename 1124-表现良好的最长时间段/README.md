48e8b6f9c1b899ad2c7929c8b22a68545cbc572a
凉风有信 <3042146237@qq.com>
2022-01-18 11:26:38 +0800

    fea: 636 && 1124

---
+++ 1124-表现良好的最长时间段/README.md
@@ -0,0 +1,3 @@
+# 1124 现良好的最长时间段
+
+兄弟们，裂开了，看了半天题解没看懂，我已经放弃了，把讲解视频链接贴上：https://www.bilibili.com/video/BV1ES4y1R7Ao?from=search&seid=3413874793255507210&spm_id_from=333.337.0.0
---
+++ 1124-表现良好的最长时间段/index.js
@@ -0,0 +1,31 @@
+/**
+ * @description 1124 表现良好的最长时间段
+ * @author 氧化氢
+ */
+
+/**
+ * @param {number[]} hours
+ * @return {number}
+ */
+var longestWPI = function(hours) {
+  var preSum = new Array(hours.length + 1).fill(0)
+  for(var i = 0; i < hours.length; i++) {
+    if(hours[i] > 8) preSum[i + 1] = preSum[i] + 1
+    else preSum[i + 1] = preSum[i] - 1 
+  }
+
+  var stack = []
+  stack.push(0)
+  for(var i = 1; i < preSum.length; i++) {
+    if(preSum[i] < preSum[stack[stack.length - 1]]) stack.push(i)
+  }
+
+  var max = 0
+  for(var i = preSum.length - 1; i > max; i--) {
+    while(stack.length && preSum[stack[stack.length - 1]] < preSum[i]) {
+      max = Math.max(max, i - stack.pop())
+    }
+  }
+
+  return max
+};
