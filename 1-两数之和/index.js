e3e6b223ac9097f0d2b5e6b261d275f62a5b8bdc
凉风有信 <3042146237@qq.com>
2022-05-03 21:32:23 +0800

    update: 1 && 34

--- 1-两数之和/index.js
+++ 1-两数之和/index.js
@@ -8,7 +8,7 @@
  * @param {number} target
  * @return {number[]}
  */
-// 方法一:
+// 方法一: 通过排序加双指针：先找到目标的两个数，然后再从原数组中找到下标即可（切记一个从前找，一个从后找，因为像 [2,2] 4 这种情况如果都从前找必然不对）
 var twoSum = function(nums, target) {
   if(nums.length === 2)  return [0, 1]
   var arr = [...nums]
@@ -27,7 +27,7 @@ var twoSum = function(nums, target) {
   return [nums.indexOf(arr[i]), nums.lastIndexOf(arr[j])]
 };
 
-// 方法二:
+// 方法二: 通过哈希表的形式，还是比较巧妙地，不用多解释，直接看代码就懂了！！！
 var twoSum = function (nums, target) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
@@ -38,3 +38,52 @@ var twoSum = function (nums, target) {
     map.set(nums[i], i);
   }
 };
+
+// 方法三：排序加二分
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * @method 排序加二分
+ */
+ var twoSum = function (nums, target) {
+  var arr = new Array(nums.length)
+  for(var i = 0; i < nums.length; i++) arr[i] = [i]
+  // 对下标数组进行排序,排序后的下标对应的原数组的值是升序的
+  arr.sort((a, b) => nums[a] - nums[b])
+
+  var ans = []
+  for(var i = 0; i < arr.length; i++) {
+    // 目前的值
+    var value = nums[arr[i]]
+    var j = binary_search(nums, arr, i+1, target-value)
+    // 没找到就 continue
+    if(j === -1) continue
+    // 找到了就去 arr 中找到对应的数组真实下标:
+    var a = arr[i]
+    var b = arr[j]
+    ans[0] = a
+    ans[1] = b
+  }
+
+  return ans
+};
+
+/**
+ * @method 无脑二分
+ * @param {*} nums 原数组
+ * @param {*} arr 排序过的下标数组
+ * @param {*} key 开始查找的位置
+ * @param {*} res 查找的值
+ */
+function binary_search(nums, arr, head, res) {
+  var tail = nums.length - 1, mid
+  while (head <= tail) {
+    mid = (tail + head) >> 1
+    if(nums[arr[mid]] === res) return mid
+    else if(nums[arr[mid]] < res) head = mid + 1
+    else tail = mid - 1
+  }
+
+  return -1
+}
--- 34-在排序数组中查找元素的第一个和最后一个位置/index.js
+++ 34-在排序数组中查找元素的第一个和最后一个位置/index.js
@@ -15,3 +15,40 @@
 
   return arr
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * @method 老夫怎么会一直偷工减料呢,今天来补上思路
+ */
+ var searchRange = function(nums, target) {
+  //  找到第一个 target 的值,如果不存在就返回 [-1, -1]
+  var i = binary_search(nums, target)
+  var ret = []
+  ret[0] = i
+  if(i === nums.length || nums[i] !== target) {
+      ret[0] = ret[1] = -1
+      return ret
+  }
+  // 因为数组是排过序的,所以找到第一个大于等于 target + 1 的下标的前一个下标就是 要找的 target 的最后一位了
+  ret[1] = binary_search(nums, target + 1) - 1
+
+  return ret
+};
+
+// 在 nums 中找到第一个 大于等于 target 的下标,如果整个数组的值都小于 target,就默认数组最后一位的下一位(就是 nums.length) 是目标下标
+function binary_search(nums, target) {
+  var head = 0, tail = nums.length - 1, mid
+  while(tail - head > 3) {
+      mid = (head + tail) >> 1
+      if(nums[mid] >= target) tail = mid
+      else head = mid
+  }
+  for(var i = head; i <= tail; i++) {
+      if(nums[i] >= target) return i
+  }
+
+  return nums.length
+}
